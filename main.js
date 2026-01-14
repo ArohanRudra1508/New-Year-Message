f3e4f07ffebe0f44a7b4657b24536134cf0fffd6
ArohanRudra1508 <msanchi08@gmail.com>
2026-01-14 18:32:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let show = () => {
     let name = document.getElementById("inp").value;
 
-    document.getElementById("msg").innerHTML = "I wish a very happy to "+name+"!";
+    document.getElementById("msg").innerHTML = "I wish a very happy new year to "+name+"!";
     
     document.getElementById("p").innerHTML = "May you get a lot of happiness this year!"
+
+    document.getElementById("img").src = "https://i.etsystatic.com/30902893/r/il/e15bb7/6956867577/il_fullxfull.6956867577_tiyz.jpg"
+
+    document.getElementById("msg").style.color = "red"
 }
 
 document.getElementById("b").addEventListener("click", show)
